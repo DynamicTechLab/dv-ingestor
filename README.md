ccbb6274d7334f7807a1b48e7d9ddea7161b94da
Xiaoxi Pang <pangxiaoxi@gmail.com>
2018-05-12 11:47:49 -0400

    Create script to run the services and improve the readme

--- README.md
+++ README.md
@@ -9,31 +9,39 @@ A microservice that is writing data to ES. We use the docker container to hold t
 > Note:
 > All of our command based on Linux/Mac 
 
-## 2. Start Ingestor Connect with ElasticSearch Using Docker
-1. Create a customized network
+## 2. The Easy Way: Start all service with script
+Just run 
 ```shell
-docker network create dvnetwork --driver=bridge
-``` 
-2. Bootstrap a Elastic Search server by using docker:
-```shell
-docker run -d --rm --name elasticsearch -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" --network dvnetwork docker.elastic.co/elasticsearch/elasticsearch:6.2.4
-```
-3. Build the docker image, run the command in your *repository root*:
-```shell
-docker build -f dev/Dockerfile -t dv-ingestor-dev --build-arg DEV_HOME=$(pwd) .
-```
-4. Run the docker image with command:
-```shell
-docker run --rm --name dv-ingestor -v $(pwd):$(pwd) -p 3000:3000 --network dvnetwork dv-ingestor-dev
+./helper.sh
 ```
 Then Open your browser and hit localhost:3000, you should be able to see the hello world!
 
-5. View the ES through Kibana
+Open another tab and hit localhost:5601, you will access the Kibana
+
+## 3. The Hard Way: Start Ingestor Connect with ElasticSearch Using Docker
+1. Create a customized network
 ```shell
-docker run -d --rm --name kibana -p 5601:5601 --network dvnetwork docker.elastic.co/kibana/kibana:6.2.4
+docker network create dv_ingestor_network --driver=bridge
+``` 
+2. Bootstrap a Elastic Search server by using docker:
+```shell
+docker run -d --rm --name elasticsearch -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" --network dv_ingestor_network docker.elastic.co/elasticsearch/elasticsearch:6.2.4
+```
+3. Build the docker image, run the command in your *repository root*:
+```shell
+docker build -f dev/Dockerfile -t dv-ingestor --build-arg DEV_HOME=$(pwd) .
+```
+4. Run the docker image with command:
+```shell
+docker run --rm --name dv-ingestor -v $(pwd):$(pwd) -p 3000:3000 --network dv_ingestor_network dv-ingestor
 ```
 
-TO BE CONTINUE...
+5. View the ES through Kibana
+```shell
+docker run -d --rm --name kibana -p 5601:5601 --network dv_ingestor_network docker.elastic.co/kibana/kibana:6.2.4
+```
+
+## 4. What is next?...
 Some useful command:
 
 curl -X POST "localhost:9200/yow/_delete_by_query" -H 'Content-Type: application/json' -d'
---
+++ helper.sh
@@ -0,0 +1,148 @@
+#/bin/bash!
+
+# Color
+INFO='\033[38;5;29m'    # Dark Green
+TIP='\033[38;5;25m'     # Ocean Blue
+ERROR='\033[38;5;124m'  # Red
+END='\033[0m'       # No Color
+
+# Virables
+REPO_NAME=dv-ingestor
+IMAGE_NAME=${REPO_NAME}
+ES_NAME=elasticsearch
+KIBANA_NAME=kibana
+# To avoid interfere with other projects in the future, when using helper running up the service, give it a isolated network
+NETWORK_NAME=dv_ingestor_network
+ES_VERSION=6.2.4
+
+# Text Block
+read -d '' instruction << EOM 
+jq(lightweight command-line JSON processor) is not installed. If you want the script fully functional, please follow the instruction:
+Please install jq with following command: 
+# brew install jq  
+The waiting time to install it maybe a little bit longer than you expect  
+EOM
+
+# Functions
+install_jq(){
+    # Check if the jq --version error output is zero length or not, if yes, then the jq is not installed
+    if [ -z "$(jq --version 2>/dev/null)" ];then
+        echo "${ERROR}$instruction${END}"
+    fi
+}
+
+# Build the image
+build_image(){
+    echo "\n${INFO}Build ${REPO_NAME} image...${END}"
+    docker build -f dev/Dockerfile -t ${REPO_NAME} --build-arg DEV_HOME=$(pwd) .
+}
+
+# Precheck image
+has_image(){
+    if [[ -z "$(docker images -q ${IMAGE_NAME})" ]];then
+        build_image
+    fi
+}
+
+# Create the network
+create_network(){
+    if [[ -z "$(docker network inspect ${NETWORK_NAME} 2>/dev/null)" ]];then
+        docker network create ${NETWORK_NAME} --driver=bridge
+    fi
+}
+
+# Start elastic search
+start_es(){
+    if [[ -z "$(docker ps -aqf "name=${ES_NAME}")" ]]; then
+        echo "${INFO}Start ElasticSearch...${END}" && \
+        docker run -d -it --rm --name ${ES_NAME} -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" --network ${NETWORK_NAME} docker.elastic.co/elasticsearch/elasticsearch:${ES_VERSION}
+    else 
+        echo "${INFO}ElasticSearch is already up...${END}"
+    fi
+}
+
+start_kibana(){
+    if [[ -z "$(docker ps -aqf "name=${KIBANA_NAME}")" ]]; then
+        echo "${INFO}Start Kibana...${END}" && \
+        docker run -d -it --rm --name ${KIBANA_NAME} -p 5601:5601 --network ${NETWORK_NAME} docker.elastic.co/kibana/kibana:${ES_VERSION} 
+    else 
+        echo "${INFO}Kibana is already up...${END}"
+    fi
+}
+
+start_app(){
+    if [[ -z "$(docker ps -aqf "name=${REPO_NAME}")" ]]; then
+        echo "${INFO}Start ${REPO_NAME}..." &&\
+        docker run --rm -it --name ${REPO_NAME} -v $(pwd):$(pwd) -p 3000:3000 --network ${NETWORK_NAME} ${IMAGE_NAME}
+    else 
+        echo "${INFO}Kibana is already up...${END}"
+    fi
+}
+
+# Start all services
+start_all(){
+    { 
+        echo "\n${TIP}TIPS: If you want to trace ES or Kibana log, open a new terminal tab and copy command:${END}" && \
+        echo "${TIP}# docker logs -f ${ES_NAME}${END}" && \
+        echo "${TIP}# docker logs -f ${KIBANA_NAME}${END}" && \
+        start_es && \
+        start_kibana && \
+        start_app
+        
+    }
+
+}
+
+
+# Stop and remove single container
+stop_and_rm_container(){
+    # Filter the specific docker container by using its name and check the output is nonzero
+    if [[ -n $(docker ps -aqf "name=$1") ]]; then
+        docker stop $1
+        # Check if the container still availabe and remove it if it is not auto removable
+        if [ $(docker inspect $1 2>/dev/null | jq '.[0].HostConfig.AutoRemove') == "false" ]; then
+            docker rm $1
+        fi
+    fi
+}
+
+# Stop and remove all of the containers
+remove_all(){
+    echo "\n${INFO}Stop and remove all services...${END}\n"
+    echo "${INFO}Stop and remove Kibana...${END}" &&\
+    stop_and_rm_container kibana
+    echo "${INFO}Stop and remove ElasticSearch...${END}" &&\
+    stop_and_rm_container elasticsearch
+    echo "${INFO}Stop and remove ${REPO_NAME}...${END}" &&\
+    stop_and_rm_container ${REPO_NAME}
+}
+
+# Main
+install_jq
+
+echo "${INFO}Select one action:"
+
+actions=(
+    "Start ${REPO_NAME} with ES and Kibana"
+    "Stop all services"
+    "Quit"
+)
+
+select action in "${actions[@]}"
+do
+    case ${action} in
+        "${actions[0]}")
+            echo "\n${INFO}Start ${REPO_NAME} with ES and Kibana...${END}"
+            has_image && \
+            create_network && \
+            start_all
+            ;;
+        "${actions[1]}")
+            remove_all
+            ;;
+        "${actions[2]}")
+            break
+            ;;
+        *) echo "${ERROR} invalid action${END}";;
+    esac
+done
